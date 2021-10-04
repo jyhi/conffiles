15e75ff28cb5823845fd42aaa4d63b8462361866
Junde Yhi <junde@yhi.moe>
2021-10-04 10:37:41 +0100

    common/scripts: add tar-cf.sh, deterministic posix tar

---
+++ common/scripts/tar-cf.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+tar --sort=name --mtime='@0' --owner=0 --group=0 --numeric-owner \
+    --format=posix --pax-option=exthdr.name=%d/PaxHeaders/%f,delete=atime,delete=ctime \
+    --create --file $@
