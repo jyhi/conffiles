c9e2e4b9842b49de5ffe4e3af8917aa18ddd3cb5
Junde Yhi <junde@yhi.moe>
2022-08-07 18:46:58 +0100

    latitude: working system update

--- latitude/configuration.nix
+++ latitude/configuration.nix
@@ -1,6 +1,6 @@
 # NixOS Configuration for <latitude.yhi.moe>.
 
-{ pkgs, ... }:
+{ lib, pkgs, ... }: 
 let
   pathImpermanence = builtins.fetchGit {
     url = "https://github.com/nix-community/impermanence";
@@ -19,21 +19,46 @@ in {
       efi.canTouchEfiVariables = true;
       generationsDir.copyKernels = true;
     };
-    supportedFilesystems = [ "f2fs" ];
-    tmpOnTmpfs = true;
-    initrd.luks.devices = {
-      "nixos" = {
-        device = "/dev/disk/by-partlabel/linux-system";
-        allowDiscards = true;
-        bypassWorkqueues = true;
+
+    initrd = {
+      availableKernelModules = [ "nvme" ];
+      luks.devices = {
+        "nixos" = {
+          device = "/dev/disk/by-partlabel/linux-system";
+          allowDiscards = true;
+          bypassWorkqueues = true;
+        };
       };
     };
+
+    supportedFilesystems = [ "f2fs" ];
+    tmpOnTmpfs = true;
+
+    # Enable Intel IOMMU to enable iGVT-g
+    # https://wiki.archlinux.org/title/Intel_GVT-g
+    kernelParams = [ "intel_iommu=on" ];
+
+    # - Enable nested virtualisation
+    # - Enable iGVT-g and HuC firmware load
+    extraModprobeConfig = ''
+      options kvm_intel nested=1
+      options i915 enable_gvt=1 enable_guc=2
+    '';
+
+    # Enable S3 sleep
+    postBootCommands = "echo deep > /sys/power/mem_sleep";
   };
 
   fileSystems = {
     "/" = {
       fsType = "tmpfs";
-      options = [ "rw" "noatime" "nodev" "nosuid" "mode=755" ];
+      options = [ "rw" "noatime" "nodev" "nosuid" "noexec" "mode=755" ];
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-partlabel/esp";
+      fsType = "vfat";
+      options = [ "rw" "noatime" "nodev" "nosuid" "noexec" ];
     };
 
     "/nix" = {
@@ -77,9 +102,12 @@ in {
   };
 
   time.timeZone = "Europe/London";
+  i18n.defaultLocale = "en_GB.UTF-8";
 
   environment = {
-    memoryAllocator.provider = "mimalloc";
+    # This breaks Firefox
+    # memoryAllocator.provider = "mimalloc";
+
     localBinInPath = true;
     sessionVariables = {
       # Configure Electron apps to run on Wayland
@@ -90,7 +118,6 @@ in {
 
   users = {
     mutableUsers = false;
-    defaultUserShell = pkgs.dash;
     users = {
       "jyhi" = {
         uid = 1001;
@@ -98,7 +125,7 @@ in {
         isNormalUser = true;
         initialHashedPassword =
           "$5$qJVeybe.0P7E9CQu$wvN4gJWLVY2U1V.sACa4.KeDw0muEY0yKW/Ez6YQaZB";
-        extraGroups = [ "wheel" "video" "networkmanager" ];
+        extraGroups = [ "wheel" "networkmanager" "libvirtd" ];
       };
     };
   };
@@ -115,6 +142,7 @@ in {
             type filter hook input priority filter; policy drop;
 
             iifname lo accept
+            iifname virbr0 accept
             ct state {established, related} accept
             ct state invalid drop
 
@@ -125,7 +153,7 @@ in {
           }
 
           chain forward {
-            type filter hook forward priority filter; policy drop;
+            type filter hook forward priority filter; policy accept;
           }
 
           chain output {
@@ -152,7 +180,7 @@ in {
     tlp = {
       enable = true;
       settings = {
-        TLP_DEFAULT_MODE = "AC";
+        TLP_DEFAULT_MODE = "BAT";
         TLP_PERSISTENT_DEFAULT = 1;
         CPU_SCALING_GOVERNOR_ON_AC = "powersave";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
@@ -173,6 +201,32 @@ in {
     };
   };
 
+  # Based on https://github.com/NixOS/nixpkgs/blob/nixos-22.05/nixos/modules/services/networking/mullvad-vpn.nix
+  # Switch from mullvad-vpn to mullvad
+  # pkgs.mullvad is installed in environment.systemPackages
+  systemd.services.mullvad-daemon = {
+    description = "Mullvad VPN daemon";
+    wantedBy = [ "multi-user.target" ];
+    wants = [ "network.target" ];
+    after = [
+      "network-online.target"
+      "NetworkManager.service"
+      "systemd-resolved.service"
+    ];
+    path = [
+      pkgs.iproute2
+      # Needed for ping
+      "/run/wrappers"
+    ];
+    startLimitBurst = 5;
+    startLimitIntervalSec = 20;
+    serviceConfig = {
+      ExecStart = "${pkgs.mullvad}/bin/mullvad-daemon -v --disable-stdout-timestamps";
+      Restart = "always";
+      RestartSec = 1;
+    };
+  };
+
   programs = {
     vim.defaultEditor = true;
 
@@ -184,7 +238,7 @@ in {
     gnupg = {
       agent = {
         enable = true;
-        pinentryFlavor = "qt";
+        pinentryFlavor = "gnome3";
       };
       dirmngr.enable = true;
     };
@@ -194,9 +248,12 @@ in {
       wrapperFeatures.gtk = true;
       extraPackages = with pkgs; [
         swaylock
+        swayidle
         waybar
         brightnessctl
         wl-clipboard
+        bluez # bluetooth
+        libayatana-appindicator # tray
         slurp # screen region selector
         grim # screenshot
         mako # notification daemon
@@ -210,48 +267,146 @@ in {
         export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
         export SDL_VIDEODRIVER=wayland
         export _JAVA_AWT_WM_NONREPARENTING=1
+
+        export GRIM_DEFAULT_DIR=$HOME/Pictures/Screenshots
       '';
+    };
+  };
+
+  virtualisation = {
+    kvmgt = {
+      enable = true;
+      vgpus = {
+        "i915-GVTg_V5_4" = {
+          uuid = [ "49d1a7d1-3b31-45f1-a2a1-5d37bd646a85" ];
+        };
+      };
+    };
+
+    libvirtd = {
+      enable = true;
+      onBoot = "ignore";
+      qemu = {
+        swtpm.enable = true;
+        runAsRoot = false;
+      };
     };
   };
 
   xdg.portal = {
     enable = true;
-    gtkUsePortal = true;
     wlr.enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+
+      # This crashes for it cannot load qt.qpa.xcb, but it should load wayland-egl
+      # xdg-desktop-portal-kde
+    ];
   };
 
-  # environment.systemPackages = with pkgs; [ ];
+  fonts = {
+    enableDefaultFonts = true;
+    fonts = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      fira-code
+      font-awesome
+    ];
+
+    fontconfig = {
+      allowBitmaps = false;
+      defaultFonts = {
+        sansSerif = [ "Noto Sans" ];
+        serif = [ "Noto Serif" ];
+        monospace = [ "Fira Code" ];
+      };
+    };
+  };
+
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5 = {
+      addons = with pkgs; [ fcitx5-rime ];
+      enableRimeData = true;
+    };
+  };
+
+  # Get rid of this small set of "recommended" packages
+  environment.defaultPackages = lib.mkForce [ ];
+
+  environment.systemPackages = with pkgs; [
+    dash
+    htop
+    mullvad
+    neofetch
+    numix-cursor-theme
+    numix-gtk-theme
+    numix-icon-theme
+    numix-icon-theme-square
+    pcmanfm-qt
+    rclone
+    unzip
+    virt-manager
+    virtiofsd
+    xdg-utils
+    zip
+  ];
+
   users.users."jyhi".packages = with pkgs; [
+    alacritty
     firefox-wayland
+    keepassxc
+    libreoffice-fresh
+    tdesktop
     thunderbird-wayland
+    vlc
     vscodium
   ];
 
   environment.persistence."/nix/pers" = {
     hideMounts = true;
-    directories = [ "/var/log" ];
+
+    directories = [
+      "/etc/nixos"
+      { directory = "/etc/NetworkManager/system-connections"; mode = "0700"; }
+      "/var/lib"
+      "/var/log"
+    ];
+
     files = [ "/etc/machine-id" ];
+
     users."jyhi" = {
       directories = [
-        {
-          directory = ".ssh";
-          mode = "0700";
-        }
-        {
-          directory = ".gnupg";
-          mode = "0700";
-        }
-        ".config/sway"
-        ".config/swaylock"
-        ".config/waybar"
-        "Applications"
+        { directory = ".cache/rclone"; mode = "0700"; }
+        { directory = ".config/dconf"; mode = "0700"; }
+        { directory = ".config/fcitx5"; mode = "0700"; }
+        { directory = ".config/htop"; mode = "0700"; }
+        { directory = ".config/keepassxc"; mode = "0700"; }
+        { directory = ".config/libreoffice"; mode = "0700"; }
+        { directory = ".config/pcmanfm-qt"; mode = "0700"; }
+        { directory = ".config/rclone"; mode = "0700"; }
+        { directory = ".config/sway"; mode = "0700"; }
+        { directory = ".config/swayidle"; mode = "0700"; }
+        { directory = ".config/swaylock"; mode = "0700"; }
+        { directory = ".config/waybar"; mode = "0700"; }
+        { directory = ".gnupg"; mode = "0700"; }
+        { directory = ".local/state/wireplumber"; mode = "0700"; }
+        { directory = ".local/share/fcitx5/rime"; mode = "0700"; }
+        { directory = ".local/share/TelegramDesktop"; mode = "0700"; }
+        { directory = ".mozilla"; mode = "0700"; }
+        { directory = ".ssh"; mode = "0700"; }
+        { directory = ".thunderbird"; mode = "0700"; }
         "Documents"
         "Downloads"
         "Music"
         "Pictures"
+        "Repositories"
         "Videos"
       ];
-      files = [ ".vimrc" ];
+
+      files = [
+        ".vimrc"
+      ];
     };
   };
 }
--- latitude/sway/scripts/dpms_toggle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-BRI="$(cat /sys/class/backlight/intel_backlight/actual_brightness)"
-
-if [ "$BRI" = "0" ]; then
-  swaymsg output \* dpms on
-else
-  swaymsg output \* dpms off
-fi
